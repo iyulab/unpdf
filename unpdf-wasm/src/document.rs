c4082e4165a966ebadcd9aaf28394fdccc3cb5fb
uj <caveman@iyulab.com>
2026-05-31 21:15:59 +0900

    fix(wasm-test): correct MINIMAL_PDF xref byte offsets (9/52/101, startxref 151)

--- unpdf-wasm/src/document.rs
+++ unpdf-wasm/src/document.rs
@@ -53,19 +53,19 @@ mod tests {
     wasm_bindgen_test_configure!(run_in_node_experimental);
 
     const MINIMAL_PDF: &[u8] = b"%PDF-1.4\n\
-        1 0 obj<</Type/Catalog/Pages 2 0 R>>endobj\n\
-        2 0 obj<</Type/Pages/Kids[3 0 R]/Count 1>>endobj\n\
-        3 0 obj<</Type/Page/MediaBox[0 0 612 792]>>endobj\n\
-        xref\n\
-        0 4\n\
-        0000000000 65535 f \n\
-        0000000009 00000 n \n\
-        0000000058 00000 n \n\
-        0000000115 00000 n \n\
-        trailer<</Size 4/Root 1 0 R>>\n\
-        startxref\n\
-        190\n\
-        %%EOF";
+1 0 obj<</Type/Catalog/Pages 2 0 R>>endobj\n\
+2 0 obj<</Type/Pages/Kids[3 0 R]/Count 1>>endobj\n\
+3 0 obj<</Type/Page/MediaBox[0 0 612 792]>>endobj\n\
+xref\n\
+0 4\n\
+0000000000 65535 f \n\
+0000000009 00000 n \n\
+0000000052 00000 n \n\
+0000000101 00000 n \n\
+trailer<</Size 4/Root 1 0 R>>\n\
+startxref\n\
+151\n\
+%%EOF";
 
     #[wasm_bindgen_test]
     fn test_page_count() {
